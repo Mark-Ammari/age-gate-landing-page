c3c4424281873380b7748e3251d21d2c11e31d88
Mark Ammari <mark.ammari@gmail.com>
2019-06-04 14:11:31 -0400

    did javascript and styling, need to add remember me and tweak code a bit

---
+++ script.js
@@ -0,0 +1,67 @@
+const month = document.querySelector("#month");
+const day = document.querySelector("#day");
+const year = document.querySelector("#year");
+const submitBtn = document.querySelector(".submitBtn");
+
+const success = document.querySelector("#success-text");
+const error = document.querySelector("#error-text");
+
+var date = new Date();
+
+var months = ['January', 'Feburary', 'March', 'April',
+'May', 'June', 'July', 'August', 'September', 'October',
+'November', 'December'
+]
+
+function populateBirthdayHandler() {
+    success.innerHTML = " "
+    error.innerHTML = " "
+    var numOfMonth = 1
+    for(var i = 1; i <= daysInMonth(month.value, year.value); i++) {
+        day.innerHTML += '<option value="' + i + '">' + i + '</option>'
+    }
+    months.forEach(m => {
+        month.innerHTML += '<option value="' + numOfMonth++ + '">' + m + '</option>'
+    });
+
+    for(var i = date.getFullYear() - 100; i <= date.getFullYear(); i++) {
+        year.innerHTML += '<option value="' + i + '">' + i + '</option>'
+    }
+}
+
+function submitFormHandler() {
+    var userDate = new Date()
+    userDate.setMonth(month.value)
+    userDate.setFullYear(year.value)
+    userDate.setDate(day.value)
+    var dateDifference = Math.abs(date - userDate)
+    var someYearsOld = Math.floor(dateDifference / 31536000000)
+    if (someYearsOld >= 21) {
+        success.innerHTML = "Welcome!"
+        error.innerHTML = " "
+        month.style.display = 'none'
+        day.style.display = 'none'
+        year.style.display = 'none'
+
+    } else {
+        error.innerHTML = "Sorry, you are not old enough to enter this website."
+    }
+}
+
+
+function daysInMonth(month, year) {
+    return new Date(year, month, 0).getDate()
+}
+
+
+month.addEventListener('change', function() {
+    day.innerHTML = ""
+    for(var i = 1; i <= daysInMonth(month.value, year.value); i++) {
+        day.innerHTML += '<option value="' + i + '">' + i + '</option>'
+    }
+})
+
+window.addEventListener('load', populateBirthdayHandler())
+
+submitBtn.addEventListener('click', submitFormHandler)
+
